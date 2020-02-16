b793e4932a06e3f6abfc1ded69d6a400607a4641
burnt43 <jms.crsn@gmail.com>
2020-02-15 23:11:12 -0500

    add some stuff

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'digits', git: 'https://github.com/burnt43/digits.git'
+gem 'method-result-caching', git:  'https://github.com/burnt43/method-result-caching.git'
--- chooglin.rb
+++ chooglin.rb
@@ -1,4 +1,34 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+require 'digits'
+require 'method-result-caching'
+
 module Chooglin
+  class << self
+    def bust_survive_hotdice_probs
+      1.upto(6) do |num_dice|
+        printf("-----------------\nNumber of Dice: %d\n", num_dice)
+
+        rolls = Roll.all(num_dice)
+
+        ways_to_bust     = 0
+        ways_to_survive  = 0
+        ways_to_hot_dice = 0
+
+        rolls.each do |roll|
+          ways_to_bust +=1 if roll.bust?
+          ways_to_survive +=1 if roll.survive?
+          ways_to_hot_dice +=1 if roll.hot_dice?
+        end
+
+        printf("%20s: %2.2f%%\n", 'Bust', 100 * ways_to_bust / rolls.size.to_f)
+        printf("%20s: %2.2f%%\n", 'Survive', 100 * ways_to_survive / rolls.size.to_f)
+        printf("%20s: %2.2f%%\n", 'Hot Dice', 100 * ways_to_hot_dice / rolls.size.to_f)
+      end
+    end
+  end
+
   class Roll
     # class methods
     class << self
@@ -9,7 +39,19 @@ module Chooglin
         self.new(*dice_values)
       end
 
-      def all
+      def all(*number_of_dice_values)
+        adjusted_values =
+          if number_of_dice_values.empty?
+            [1, 2, 3, 4, 5, 6]
+          else
+            number_of_dice_values
+          end
+
+        adjusted_values.each_with_object([]) do |num_digits, array|
+          Digits::Generator.each_digit_combo(num_digits, base: 6, offset: 1) do |*digit_values|
+            array.push( Roll.new(*digit_values) )
+          end
+        end
       end
     end
 
@@ -24,14 +66,26 @@ module Chooglin
       @dice_values.size
     end
 
-    def num_scoring_dice
+    def bust?
+      subsets.select(&:valid_for_scoring?).size == 0
     end
 
-    def all_scores
+    def survive?
+      subsets.select(&:valid_for_scoring?).any? {|subset| subset.scored?}
     end
 
     def hot_dice?
+      subsets.select(&:valid_for_scoring?).any? {|subset| subset.size == size}
     end
+
+    def subsets
+      size.downto(1).flat_map do |subset_size|
+        @dice_values.combination(subset_size).map do |dice_combo|
+          Subset.new(*dice_combo)
+        end
+      end
+    end
+    cache_result_for :subsets
 
     def to_s
       "<##{self.class.name} @dice_values=#{@dice_values.to_s}>"
@@ -45,6 +99,24 @@ module Chooglin
       end
 
       # instance methods
+      def size
+        @dice_values.size
+      end
+
+      def valid_for_scoring?
+        !scores.empty? &&
+        scores.map(&:points).reduce(:+) > 0
+        size == num_scoring_dice
+      end
+
+      def num_scoring_dice
+        scores.map(&:num_dice_needed).reduce(:+)
+      end
+
+      def scored?
+        !scores.empty?
+      end
+
       def scores
         values     = @dice_values.clone
         check_size = values.size
@@ -200,6 +272,7 @@ module Chooglin
 
         result
       end
+      cache_result_for :scores
 
       def to_s
         "<##{self.class.name} @dice_values=#{@dice_values.to_s}>"
@@ -213,6 +286,76 @@ module Chooglin
       end
 
       # instance methods
+      def points
+        case @type
+        when :six_ones     then 1000 * 8
+        when :six_twos     then 200 * 8
+        when :six_threes   then 300 * 8
+        when :six_fours    then 400 * 8
+        when :six_fives    then 500 * 8
+        when :six_sixes    then 600 * 8
+        when :unique       then 1500
+        when :three_pair   then 1000
+        when :five_ones    then 1000 * 4
+        when :five_twos    then 200 * 4
+        when :five_threes  then 300 * 4
+        when :five_fours   then 400 * 4
+        when :five_fives   then 500 * 4
+        when :five_sixes   then 600 * 4
+        when :four_ones    then 1000 * 2
+        when :four_twos    then 200 * 2
+        when :four_threes  then 300 * 2
+        when :four_fours   then 400 * 2
+        when :four_fives   then 500 * 2
+        when :four_sixes   then 600 * 2
+        when :three_ones   then 1000
+        when :three_twos   then 200
+        when :three_threes then 300
+        when :three_fours  then 400
+        when :three_fives  then 500
+        when :three_sixes  then 600
+        when :single_one   then 100
+        when :single_five  then 50
+        else
+          0
+        end
+      end
+
+      def num_dice_needed
+        case @type
+        when :six_ones     then 6
+        when :six_twos     then 6
+        when :six_threes   then 6
+        when :six_fours    then 6
+        when :six_fives    then 6
+        when :six_sixes    then 6
+        when :unique       then 6
+        when :three_pair   then 6
+        when :five_ones    then 5
+        when :five_twos    then 5
+        when :five_threes  then 5
+        when :five_fours   then 5
+        when :five_fives   then 5
+        when :five_sixes   then 5
+        when :four_ones    then 4
+        when :four_twos    then 4
+        when :four_threes  then 4
+        when :four_fours   then 4
+        when :four_fives   then 4
+        when :four_sixes   then 4
+        when :three_ones   then 3
+        when :three_twos   then 3
+        when :three_threes then 3
+        when :three_fours  then 3
+        when :three_fives  then 3
+        when :three_sixes  then 3
+        when :single_one   then 1
+        when :single_five  then 1
+        else
+          0
+        end
+      end
+
       def to_s
         "<##{self.class.name} @type=#{@type}>"
       end
@@ -224,4 +367,18 @@ end
 #   puts Chooglin::Roll.random(6)
 # end
 
-puts Chooglin::Roll::Subset.new(2,2,3,3,2,3).scores
+# tab = "  "
+# roll = Chooglin::Roll.new(1,2,3,4,5,6)
+# puts roll.to_s
+# roll.subsets.select(&:valid_for_scoring?).each do |subset|
+#   puts "#{tab}#{subset.to_s}"
+#   subset.scores.each do |score|
+#     puts "#{tab*2}#{score.to_s} #{score.points}"
+#   end
+# end
+
+Chooglin.bust_survive_hotdice_probs
+
+# Chooglin::Roll.all(4).each do |roll|
+#   puts roll
+# end
